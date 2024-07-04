c4e7d702c637d6de263faf5b3da92b71a05735fa
nvnsudharsan <nvnsuhdarsan@gmail.com>
2024-07-04 16:54:41 -0500

    change in code

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -13,11 +13,14 @@ st.title("Hurricane Beryl Forecast")
 st.write('Experimental Forecast from UT Jackson School of Geosciences with TACC resources using ongoing research models IOLA (with NOAA and India) and Graphcast Operational setup at UT')
 # Display the first GIF
 st.header("Hurricane Beryl forecast from IOLA (Initialization: 2024-07-02 12:00:00 UTC)")
-st.image("IOLA.gif")
+st.markdown(
+    f'<img src="data:IOLA.gif;base64,{data_url}" alt="IOLA.gif">',
+    unsafe_allow_html=True,
+)
 
 # Display the second GIF
 st.header("Hurricane Beryl forecast from Graphcast Operational (Initialization: 2024-07-04 00:00:00 UTC)")
-st.image("grpahcast.gif")
+st.image("graphcast.gif")
 
 st.header('NHC advisory released on July 4, 2024 5PM EDT')
 st.image('NHC_cone.png')
