7a5e942d5e77cccfe454bcf63e4b8c9221b2281c
nvnsudharsan <nvnsuhdarsan@gmail.com>
2024-07-04 15:49:25 -0500

    Initial commit

---
+++ hurricane_beryl_streamlit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 18:18:42 2024
+
+@author: geo-ns36752
+"""
+
+import streamlit as st
+
+# Set the title of the app
+st.title("Hurricane Beryl Forecast")
+st.write('Experimental Forecast from UT Jackson School of Geosciences with TACC resources using ongoing research models IOLA (with NOAA and India) and Graphcast Operational setup at UT')
+# Display the first GIF
+st.header("Hurricane Beryl forecast from IOLA (Initialization: 2024-07-02 12:00:00 UTC)")
+st.image("IOLA.gif")
+
+# Display the second GIF
+st.header("Hurricane Beryl forecast from Graphcast Operational (Initialization: 2024-07-04 00:00:00 UTC)")
+st.image("grpahcast.gif")
+
+st.header('NHC advisory released on July 4, 2024 5PM EDT')
+st.image('NHC_cone.png')
