ea75f0b98d6b046c930811f5d21f5e1c51273f48
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2024-07-04 23:43:58 -0500

    Update hurricane_beryl_streamlit.py

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -30,7 +30,7 @@ def displayLocalGIF1(placeholder, localImagePath, caption):
     
     # Display the GIF and caption with positioning relative to the placeholder
     placeholder.markdown(f"""<div style="{container_style}">
-                    <img src="data:image/gif;base64,{imgData}" width='1000' height='600'>
+                    <img src="data:image/gif;base64,{imgData}"> #<img src="data:image/gif;base64,{imgData}" width='1000' height='600'>
                     <p style="{caption_style}">{caption}</p>
                     </div>""", unsafe_allow_html=True)
 
