079231635e048d9f9974fe815cb6ace024160674
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2024-07-05 00:51:25 -0500

    Update hurricane_beryl_streamlit.py

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -10,7 +10,7 @@ import streamlit as st
 from PIL import Image
 import base64
 
-def displayLocalGIF1(placeholder, localImagePath, caption):
+def displayLocalGIF(placeholder, localImagePath, caption):
     imgFile  = open(localImagePath, "rb")
     contents = imgFile.read()
     imgData  = base64.b64encode(contents).decode("utf-8")
@@ -35,29 +35,6 @@ def displayLocalGIF1(placeholder, localImagePath, caption):
                     </div>""", unsafe_allow_html=True)
 
 
-def displayLocalGIF2(placeholder, localImagePath, caption):
-    imgFile  = open(localImagePath, "rb")
-    contents = imgFile.read()
-    imgData  = base64.b64encode(contents).decode("utf-8")
-    imgFile.close()
-    
-    # Define CSS styles for the container and caption
-    container_style = (
-        "position: relative;"       # Enable relative positioning
-        "display: inline-block;"    # Display as inline-block to align with placeholder
-    )
-    
-    caption_style = (
-        "font-size: 14px;"      # Adjust the font size as needed
-        "color: #888888;"       # Dimmer color
-        "text-align: center;"   # Center the caption text
-    )
-    
-    # Display the GIF and caption with positioning relative to the placeholder
-    placeholder.markdown(f"""<div style="{container_style}">
-                    <img src="data:image/gif;base64,{imgData}">
-                    <p style="{caption_style}">{caption}</p>
-                    </div>""", unsafe_allow_html=True)
 
 
 
@@ -69,13 +46,17 @@ st.write('Experimental Forecast from UT Austin, Jackson School of Geosciences wi
 st.markdown("<H3 style='text-align: center;'>Hurricane Beryl forecast from IOLA</H3>", unsafe_allow_html=True)
 image_placeholder1 = st.empty()
 imagePath1 = "IOLA.gif"
-displayLocalGIF1(image_placeholder1, imagePath1,"Initialization: 2024-07-02 12:00:00 UTC")
+displayLocalGIF(image_placeholder1, imagePath1,"Initialization: 2024-07-02 12:00:00 UTC")
 
 # Display the second GIF
 st.markdown("<H3 style='text-align: center;'>Hurricane Beryl forecast from Graphcast Operational</H3>", unsafe_allow_html=True)
 image_placeholder2 = st.empty()
-imagePath2 = "graphcast.gif"
-displayLocalGIF2(image_placeholder2, imagePath2,"Initialization: 2024-07-04 00:00:00 UTC")
+imagePath2 = "graphcast1.gif"
+displayLocalGIF(image_placeholder2, imagePath2,"Initialization: 2024-07-04 00:00:00 UTC")
+
+image_placeholder3 = st.empty()
+imagePath3 = "graphcast2.gif"
+displayLocalGIF(image_placeholder3, imagePath3,"Initialization: 2024-07-05 00:00:00 UTC")
 
 st.markdown("<H3 style='text-align: center;'>National Hurricane Center advisory</H3>", unsafe_allow_html=True)
 st.image('https://www.nhc.noaa.gov/storm_graphics/AT02/refresh/AL022024_5day_cone_no_line_and_wind+png/024651_5day_cone_no_line_and_wind.png')
