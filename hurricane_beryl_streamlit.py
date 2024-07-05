e431258d15bc8589ace16b74fac537deded51806
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2024-07-04 22:41:33 -0500

    Update hurricane_beryl_streamlit.py
    gif edits

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -7,23 +7,16 @@ Created on Mon Jun  3 18:18:42 2024
 """
 
 import streamlit as st
-
-def displayLocalGIF(placeholder, imagePath, caption):
-    placeholder.image(
-        imagePath,
-        use_column_width=False,  # Disable container width
-        width=100,               # Set the width
-        caption=caption       # Optional caption
-    )
+from PIL import Image
+import base64
 
 # Set the title of the app
 st.title("Hurricane Beryl Forecast")
 st.write('Experimental Forecast from UT Jackson School of Geosciences with TACC resources using ongoing research models IOLA (with NOAA and India) and Graphcast Operational setup at UT')
 # Display the first GIF
 st.header("Hurricane Beryl forecast from IOLA (Initialization: 2024-07-02 12:00:00 UTC)")
-image_placeholder1 = st.empty()
-imagePath1 = "https://github.com/nvnsudharsan/hurricane_beryl_forecast/blob/main/IOLA.gif"
-displayLocalGIF(image_placeholder1, imagePath1,"Local Image")
+mygif = base64.b64encode(open("IOLA.gif", 'rb').read()).decode()
+st.markdown(f"""<img src="data:png;base64,{mygif}" width='50' height='50' >""", True)
 
 # Display the second GIF
 st.header("Hurricane Beryl forecast from Graphcast Operational (Initialization: 2024-07-04 00:00:00 UTC)")
