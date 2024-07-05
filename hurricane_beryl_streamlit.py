6ea754bcdda9907d81a770cb62d5e337e66451b1
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2024-07-04 22:46:01 -0500

    Update hurricane_beryl_streamlit.py

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -10,13 +10,49 @@ import streamlit as st
 from PIL import Image
 import base64
 
+def displayLocalGIF1(placeholder, imagePath, caption):
+    placeholder.image(
+        imagePath,
+        use_column_width=False,  # Disable container width
+        width=100,               # Set the width
+        caption=caption          # Optional caption
+    )
+
+def displayLocalGIF2(placeholder, localImagePath, caption):
+    imgFile  = open(localImagePath, "rb")
+    contents = imgFile.read()
+    imgData  = base64.b64encode(contents).decode("utf-8")
+    imgFile.close()
+    
+    # Define CSS styles for the container and caption
+    container_style = (
+        "position: relative;"       # Enable relative positioning
+        "display: inline-block;"    # Display as inline-block to align with placeholder
+    )
+    
+    caption_style = (
+        "font-size: 14px;"      # Adjust the font size as needed
+        "color: #888888;"       # Dimmer color
+        "text-align: center;"   # Center the caption text
+    )
+    
+    # Display the GIF and caption with positioning relative to the placeholder
+    placeholder.markdown(f"""<div style="{container_style}">
+                    <img src="data:image/gif;base64,{imgData}" width='100' height='100'>
+                    <p style="{caption_style}">{caption}</p>
+                    </div>""", unsafe_allow_html=True)
+
+
+
 # Set the title of the app
 st.title("Hurricane Beryl Forecast")
 st.write('Experimental Forecast from UT Jackson School of Geosciences with TACC resources using ongoing research models IOLA (with NOAA and India) and Graphcast Operational setup at UT')
 # Display the first GIF
 st.header("Hurricane Beryl forecast from IOLA (Initialization: 2024-07-02 12:00:00 UTC)")
-mygif = base64.b64encode(open("IOLA.gif", 'rb').read()).decode()
-st.markdown(f"""<img src="data:png;base64,{mygif}" width='50' height='50' >""", True)
+image_placeholder1 = st.empty()
+imagePath1 = "IOLA.gif"
+displayLocalGIF2(image_placeholder1, imagePath1,"Local Image")
+#image_placeholder2 = st.empty()
 
 # Display the second GIF
 st.header("Hurricane Beryl forecast from Graphcast Operational (Initialization: 2024-07-04 00:00:00 UTC)")
