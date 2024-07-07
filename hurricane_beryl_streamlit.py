714931ce609d2f7f961153e70a9829fa92ae627f
nvnsudharsan <nvnsuhdarsan@gmail.com>
2024-07-07 00:22:22 -0500

    updates in forecast

--- hurricane_beryl_streamlit.py
+++ hurricane_beryl_streamlit.py
@@ -45,29 +45,43 @@ st.write('Experimental Forecast from UT Austin, Jackson School of Geosciences wi
 # Display the first GIF
 st.markdown("<H3 style='text-align: center;'>Hurricane Beryl forecast from Experimental HWRFx</H3>", unsafe_allow_html=True)
 image_placeholder1 = st.empty()
-imagePath1 = "IOLA.gif"
-displayLocalGIF(image_placeholder1, imagePath1,"Initialization: 2024-07-06 00:00:00 UTC")
+imagePath1 = "IOLA1.gif"
+displayLocalGIF(image_placeholder1, imagePath1,"Initialization: 2024-07-06 12:00:00 UTC")
+
+image_placeholder8 = st.empty()
+imagePath8 = "IOLA2.gif"
+displayLocalGIF(image_placeholder8, imagePath8,"Initialization: 2024-07-06 12:00:00 UTC")
 
 # Display the second GIF
 st.markdown("<H3 style='text-align: center;'>Hurricane Beryl forecast from Graphcast Operational</H3>", unsafe_allow_html=True)
 image_placeholder2 = st.empty()
 imagePath2 = "graphcast1.gif"
-displayLocalGIF(image_placeholder2, imagePath2,"Initialization: 2024-07-06 00:00:00 UTC")
+displayLocalGIF(image_placeholder2, imagePath2,"Initialization: 2024-07-07 00:00:00 UTC")
 
 image_placeholder3 = st.empty()
 imagePath3 = "graphcast2.png"
-displayLocalGIF(image_placeholder3, imagePath3,"Initialization: 2024-07-06 00:00:00 UTC")
+displayLocalGIF(image_placeholder3, imagePath3,"Initialization: 2024-07-07 00:00:00 UTC")
 st.write('The forecast shows that the cumulative precipitation in and around Austin will be less than 1.5 inches.')
 
 st.markdown("<H3 style='text-align: center;'>Previous Forecasts</H3>", unsafe_allow_html=True)
 st.markdown("<H5 style='text-align: center;'>Graphcast Operational</H5>", unsafe_allow_html=True)
+
 image_placeholder4 = st.empty()
-imagePath4 = "graphcast1_old.gif"
+imagePath6 = "graphcast1_old.gif"
+displayLocalGIF(image_placeholder6, imagePath6,"Initialization: 2024-07-06 00:00:00 UTC")
+
+image_placeholder4 = st.empty()
+imagePath4 = "graphcast1_older.gif"
 displayLocalGIF(image_placeholder4, imagePath4,"Initialization: 2024-07-05 00:00:00 UTC")
 
 st.markdown("<H5 style='text-align: center;'>Experimental HWRFx</H5>", unsafe_allow_html=True)
+
+image_placeholder7 = st.empty()
+imagePath7 = "IOLA1_old.gif"
+displayLocalGIF(image_placeholder7, imagePath7,"Initialization: 2024-07-06 00:00:00 UTC")
+
 image_placeholder5 = st.empty()
-imagePath5 = "IOLA_old.gif"
+imagePath5 = "IOLA1_older.gif"
 displayLocalGIF(image_placeholder5, imagePath5,"Initialization: 2024-07-02 12:00:00 UTC")
 
 
